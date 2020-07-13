74e170b035d7fe936f6627b4ea64dd85dafbb94b
NedosekinStas <stasnedosekin@mail.ru>
2020-07-13 19:45:20 +0300

    initial db function

--- app.rb
+++ app.rb
@@ -1,8 +1,28 @@
 require 'rubygems'
 require 'sinatra'
+require 'sinatra/reloader'
+require 'sqlite3'
+
+def init_db
+  @db = SQLite3::Database.new 'lepra.db'
+  @db.results_as_hash = true
+end
+
+before do
+  init_db
+end
 
 configure do
   enable :sessions
+end
+
+get '/new/' do
+ erb :new
+end
+
+post '/new' do
+  inputText = params[:addNewPost]
+  erb "Введено: #{inputText}"
 end
 
 helpers do
