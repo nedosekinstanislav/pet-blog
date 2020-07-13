c470a6942fb8fd55ac3f51ff038aca5da3145245
NedosekinStas <stasnedosekin@mail.ru>
2020-07-13 20:04:22 +0300

    add comments to cod

--- app.rb
+++ app.rb
@@ -8,12 +8,16 @@ def init_db
   @db.results_as_hash = true
 end
 
+# Вызывается каждый раз при перезагрузке страницы
 before do
-  init_db
+  init_db # Инициализация БД
 end
 
+# Вызывается каждый раз при конфигурации приложения
+# Изменился код либо перезагрузилась страница
 configure do
-  init_db 
+  init_db
+  # Создается таблица если таблица не существует 
   @db.execute 'CREATE TABLE IF NOT EXISTS "Posts" ("id" INTEGER PRIMARY KEY AUTOINCREMENT, "created_date" DATE, "content" TEXT)'
 end
 
@@ -21,10 +25,12 @@ configure do
   enable :sessions
 end
 
+# Браузер получает страницу с сервера
 get '/new/' do
  erb :new
 end
 
+# Браузер отправляет страницу с сервера
 post '/new' do
   inputText = params[:addNewPost]
   erb "Введено: #{inputText}"
